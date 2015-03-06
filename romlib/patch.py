0b74e0bf4eb80e03db1fde9e3f571da38d9d8464
Andrew Vant <ajvant@gmail.com>
2015-03-05 20:59:24 -0500

    Added classmethods to dump and recompile ips patches.

--- romlib/patch.py
+++ romlib/patch.py
@@ -1,3 +1,5 @@
+import binascii
+import io
 from bitstring import Bits
 
 class IPSPatch(object):
@@ -42,3 +44,78 @@ class IPSPatch(object):
             recordbytes += Bits(uintbe=len(data), length=16).bytes
             recordbytes += data
         f.write(self.header + recordbytes + self.footer)
+
+    @classmethod
+    def textualize(cls, infile, outfile):
+        """ Convert an IPS patch to a readable text format.
+
+        Note that this intentionally does not validate the structure of the
+        input file. This is so you can see and possibly manually repair errors.
+        """
+
+        # Not sure if I should do my own seeking here or assume the caller did
+        # the right thing....
+        infile.seek(0)
+        outfile.seek(0)
+
+        # Convert the header
+        print(infile.read(5).decode(), file=outfile)
+
+        # Read and print records until we hit EOF.
+        while True:
+            offset_raw = infile.read(3)
+
+            if offset_raw == b'EOF':
+                print("EOF", file=outfile)
+                continue # Allow brokenness, e.g. bogobyte.
+
+            if not offset_raw:
+                break # End of file
+
+            size_raw = infile.read(2)
+            size = int.from_bytes(size_raw, 'big')
+            # If size is zero, we're dealing with an RLE record.
+            repeat_raw = None if size else infile.read(2)
+            repeat = None if size else int.from_bytes(repeat_raw, 'big')
+            data_raw = infile.read(size) if size else infile.read(1)
+
+            # Data and offset need some massaging to get output strings.
+            data_str = binascii.hexlify(data_raw).decode().upper()
+            offset_str = binascii.hexlify(offset_raw).decode().upper()
+
+            if repeat is None:
+                line = "0x{}:{}:{}".format(offset_str, size, data_str)
+                print(line, file=outfile)
+            else:
+                line = "0x{}:!{}:{}".format(offset_str, repeat, data_str)
+                print(line, file=outfile)
+
+
+    @classmethod
+    def compile(cls, infile, outfile):
+        infile.seek(0)
+        outfile.seek(0)
+
+        lines = (line.rstrip("\n") for line in infile)
+        for line in lines:
+            # Skip comments and blank lines
+            if not line or line.startswith("#"):
+                continue
+
+            # Lines that aren't records get output as-is.
+            if ":" not in line:
+                outfile.write(line.encode())
+                continue
+
+            # Split a record line on a colon delimiter, interpret, and write it.
+            offset, size, data = line.split(":")
+            repeat = None
+            if size.startswith("!"):
+                repeat = size[1:]
+                size = "0"
+
+            outfile.write(bytearray.fromhex(offset[2:]))
+            outfile.write(int(size, 0).to_bytes(2, 'big'))
+            if repeat:
+                outfile.write(int(repeat, 0).to_bytes(2, 'big'))
+            outfile.write(bytearray.fromhex(data))
--- tests/test_patch.py
+++ tests/test_patch.py
@@ -1,6 +1,7 @@
 import unittest
 from collections import OrderedDict
 from tempfile import TemporaryFile, NamedTemporaryFile
+from io import BytesIO, StringIO
 
 import romlib
 from romlib import patch
@@ -53,3 +54,40 @@ class TestIPSPatch(unittest.TestCase):
             patch.IPSPatch(changes).write(f)
             f.seek(0)
             self.assertEqual(f.read(), intended_output)
+
+    def test_ips_textualize(self):
+        p = BytesIO(b'PATCH\x00\x00\x00\x00\x01\x03'
+                    b'\x00\x00\x01\x00\x04\x01\x01\x01\xAAEOF')
+        text = "PATCH\n0x000000:1:03\n0x000001:4:010101AA\nEOF\n"
+        result = StringIO("")
+        patch.IPSPatch.textualize(p, result)
+        self.assertEqual(result.getvalue(), text)
+
+    def test_ips_textualize_rle(self):
+        p = BytesIO(b'PATCH\x00\x00\x00\x00\x00\x00\x02\xFFEOF')
+        text = "PATCH\n0x000000:!2:FF\nEOF\n"
+        result = StringIO("")
+        patch.IPSPatch.textualize(p, result)
+        self.assertEqual(result.getvalue(), text)
+
+    def test_ips_compile(self):
+        p = bytes(b'PATCH\x00\x00\x00\x00\x01\x03'
+                  b'\x00\x00\x01\x00\x04\x01\x01\x01\xAAEOF')
+        text = StringIO("PATCH\n0x000000:1:03\n0x000001:4:010101AA\nEOF\n")
+        result = BytesIO()
+        patch.IPSPatch.compile(text, result)
+        self.assertEqual(result.getvalue(), p)
+
+    def test_ips_compile_rle(self):
+        p = bytes(b'PATCH\x00\x00\x00\x00\x00\x00\x02\xFFEOF')
+        text = StringIO("PATCH\n0x000000:!2:FF\nEOF\n")
+        result = BytesIO()
+        patch.IPSPatch.compile(text, result)
+        self.assertEqual(result.getvalue(), p)
+
+    def test_ips_compile_line_skip(self):
+        p = bytes(b'PATCH\x00\x00\x00\x00\x00\x00\x02\xFFEOF')
+        text = StringIO("PATCH\n#\n\n0x000000:!2:FF\nEOF\n")
+        result = BytesIO()
+        patch.IPSPatch.compile(text, result)
+        self.assertEqual(result.getvalue(), p)
