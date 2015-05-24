f58f7564c279b4eb3bd25fe9320091fd6ac25db2
Andrew Vant <ajvant@gmail.com>
2015-05-24 16:12:51 -0400

    Added pep8 compliance to tests.

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 python3-coverage run --branch -m unittest discover -s tests $1 && \
-	python3-coverage report -m --include './*'
+	python3-coverage report -m --include './*' && \
+	python3 -m pep8 romlib/*.py
