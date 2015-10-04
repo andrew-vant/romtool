fc087fc2c158ad0b1e7bbcef0d8883edd5a43820
Andrew Vant <ajvant@gmail.com>
2015-10-04 10:07:55 -0400

    Added setup.py and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os
+import os.path
+from os.path import dirname, join
+from setuptools import setup, find_packages
+from pprint import pprint
+
+def read(relative_path):
+    with open(join(dirname(__file__), relative_path)) as f:
+        return f.read()
+
+setup(name="romlib",
+      version="0.1.0a1",
+      packages=find_packages("src") + ["romlib-maps"],
+      package_dir={'': "src", "romlib-maps": "data/maps"},
+      include_package_data=True,
+      install_requires=["pyyaml>=3.10", "bitstring>=3.1.3", "patricia-trie>=10"],
+      author="Andrew Vant",
+      author_email="ajvant@gmail.com",
+      description="Library and tool for manipulating video game ROMs.",
+      long_description=read("README.md"),
+      keywords="rom roms snes",
+      url="https://github.com/andrew-vant/romlib",
+      test_suite="tests",
+      entry_points={"console_scripts": ["romtool = romtool.cli:main"]},
+)
