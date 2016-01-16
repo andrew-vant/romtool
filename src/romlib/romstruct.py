1d61fc0ce495c4b185e8fea0a03f29cdcd4812d0
Andrew Vant <ajvant@gmail.com>
2016-01-16 13:15:07 -0500

    More refactoring...

--- src/romlib/romstruct.py
+++ src/romlib/romstruct.py
@@ -2,21 +2,20 @@
 
 from collections import OrderedDict
 
-from bitstring import Bits
+from bitstring import *
 
 from . import util
 
 
-class RomStruct(object):
+class Struct(object):
     """ Base class for ROM structures -- mostly table entries.
 
-    Subclasses should override the three class variables below; they specify
+    Subclasses should override the class variables below; they specify
     the fields of a given structure type.
     """
     _fields = OrderedDict()  # All fields in the struct.
     _data = OrderedDict()    # Fields that are physically part of the struct.
     _links = OrderedDict()   # Fields that are pointed to by another field.
-    _codecs = dict()         # Text tables to be used for string conversions.
 
     def __init__(self, d):
         """Create a new structure from a dictionary.
@@ -35,22 +34,78 @@ class RomStruct(object):
             setattr(self, field.id, value)
 
     @classmethod
-    def read(cls, bs):
+    def from_bitstream(cls, bs, offset=None):
         """ Read in a new structure from a bitstream.
 
-            `bs` must be a BitStream or ConstBitStream. bs.pos must be set
-            before calling this function.
-
-            In particular, anything indexed by bytes rather than bits, such as
-            a file object, will not do what you want.
+            `bs` must be a BitStream or ConstBitStream. If offset is provided,
+            it must be specified in bits and reading will begin from there.
+            Otherwise, reading will begin from bs.pos.
         """
-        data = {field.id: field.read(bs)
+        if offset is not None:
+            bs.pos = offset
+        data = {field.id: field.from_bitstream(bs)
                 for field in cls._data}
         for field in cls._links:
             pointer = cls._fields[field.pointer]
             bs.pos = (data[pointer.id] + pointer.mod) * 8
-            data[field.id] = field.read(bs)
-        return RomStruct(data)
+            data[field.id] = field.from_bitstream(bs)
+        return Struct(data)
+
+    @classmethod
+    def from_file(cls, f, offset=None):
+        """ Read in a new structure from a file object
+
+        `f` must be a file object opened in binary mode. If offset is provided,
+        it must be specified in bytes and reading will begin from there.
+        Otherwise, reading will begin from f.tell()
+        """
+        if offset is None:
+            offset = f.tell()
+        bit_offset = offset * 8
+        bs = ConstBitStream(f)
+        return cls.from_bitstream(bs, bit_offset)
+
+    def changeset(self, f=None, offset=None):
+        """ Get an offset-to-byte-value dict.
+
+        If file object `f` is provided, the dict will be filtered such that
+        bytes that do not need to be changed will not be included.
+
+        `offset` indicates the starting point of the structure. If omitted, it
+        will default to f.tell() if f was provided or 0 if it was not.
+        """
+
+        changes = {}
+        # Deal with regular data fields first. These are expected to all be
+        # bitstring-supported types because I've yet to see a ROM that wasn't
+        # that way.
+        if offset is None:
+            offset = f.tell() if f is not None else 0
+        bs = BitStream()
+        for field in self._data:
+            value = getattr(self, field.id)
+            bs.append(field.to_bits(value))
+        for i, byte in enumerate(bs.bytes):
+            changes[offset+i] = byte
+
+        # Deal with pointers.
+        for field in self._links:
+            value = getattr(self, field.id)
+            offset = getattr(self, field.pointer)
+            bits = field.to_bits(value)
+            for i, byte in enumerate(bits.bytes):
+                changes[offset+i] = byte
+
+        # Filter the changes against `f` if necessary.
+        if f is not None:
+            for offset, byte in changes.items():
+                f.seek(offset)
+                oldval = int.from_bytes(f.read(1), 'little')
+                if oldval == byte:
+                    del changes[offset]
+
+        # Done
+        return changes
 
     @staticmethod
     def dictify(*romstructs):
@@ -80,7 +135,7 @@ class Field(object):  # pylint: disable=too-many-instance-attributes
 
     For example, a 3-byte integer or a delimiter-terminated string.
     """
-    def __init__(self, odict):
+    def __init__(self, odict, ttable=None, available_tts=None):
         self.id = odict['id']  # pylint: disable=invalid-name
         self.label = odict['label']
         self.size = util.tobits(odict['size'])
@@ -95,19 +150,39 @@ class Field(object):  # pylint: disable=too-many-instance-attributes
         # Utility properties not in input
         self.bitsize = util.tobits(odict['size'])
         self.bytesize = util.divup(self.bitsize, 8)
+        # FIXME: should probably raise an exception if someone asks for a
+        # string type without a text table.
+        if ttable is None and available_tts is not None:
+            ttable = available_tts[self.display]
+        self.ttable = ttable
 
-    def read(self, bs, ttables=None, default_tt=None):
-        """ Read a field from some data source and return its value.
+    def from_file(self, f, offset=None):
+        """ Read a field from a byte offset within a file.
 
-        `bs` should be a BitStream or ConstBitStream. bs.pos must be set before
-        calling this function. The object returned will be an integer or
-        string, as appropriate.
+        Obviously this only works if a field starts somewhere byte-aligned. If
+        `offset` is not provided, it will be read from f.tell().
+
+        The returned value will be a string or an int, as appropriate.
         """
-        if ttables is None:
-            ttables = {}
+        if offset is None:
+            offset = f.tell()
+        bitoffset = offset * 8
+        bs = ConstBitStream(f)
+        return self.from_bitstream(bs, bitoffset)
+
+    def from_bitstream(self, bs, offset=None):
+        """ Read a field from a bit offset within a bitstream.
+
+        If `offset` is not provided, bs.pos will be used.
+
+        The returned value will be a string or an int, as appropriate.
+        """
+        if offset is not None:
+            bs.pos = offset
+
         if self.type == "strz":
-            ttable = ttables.get(self.display, default_tt)
-            return ttable.readstr(bs)
+            # FIXME: Pretty sure this won't work.
+            return self.ttable.readstr(bs)
         else:
             try:
                 fmt = "{}:{}".format(self.type, self.bitsize)
@@ -116,23 +191,20 @@ class Field(object):  # pylint: disable=too-many-instance-attributes
                 msg = "Field '{}' of type '{}' isn't a valid type?"
                 raise ValueError(msg, self.id, self.type)
 
-    def write(self, bs, value, ttables=None, default_tt=None):
-        """ Write a field to some data destination.
 
-        `bs` must be a BitStream, and bs.pos must be set before calling this
-        function.
-
-        In particular, if you pass a file object, it will not do what you
-        expect.
-        """
-        if ttables is None:
-            ttables = {}
+    def to_bits(self, value):
+        """ Convert a value to a Bits object."""
         if self.type == "strz":
-            ttable = ttables.get(self.display, default_tt)
-            bs.overwrite(Bits(ttable.encode(value)))
+            return Bits(self.ttable.encode(value))
         else:
-            bits = Bits('{}:{}={}'.format(self.type, self.size, value))
-            bs.overwrite(bits)
+            return Bits("{}:{}={}".format(self.type, self.size, value))
+
+    def to_bytes(self, value):
+        """ Convert a value to a bytes object.
+
+        This may fail if the field is not a whole number of bytes long.
+        """
+        return self.to_bits(value).bytes
 
     def fullorder(self, origin_sequence_order=0):
         """ Get the sort order of this field.
@@ -176,21 +248,25 @@ class Field(object):  # pylint: disable=too-many-instance-attributes
             value -= self.mod
         return value
 
+
 def define(name, fdefs, texttables):
     """ Create a new structure type.
 
     `fdefs` should be an iterable. It may contain either Field objects or any
     type that can be used to initialize a Field object (usually dicts).
     """
+    fields = []
+    for fdef in fdefs:
+        if isinstance(fdef, Field):
+            fields.append(fdef)
+        else:
+            fields.append(Field(fdef, available_tts=texttables))
 
-    fields = [Field(fd) if not isinstance(fd, Field) else fd
-              for fd in fdefs]
     clsvars = {
         '_fields': fields,
         '_links': [f for f in fields if f.pointer],
         '_data': [f for f in fields if not f.pointer],
-        '_codecs': {tt.name: tt for tt in texttables}
         }
 
-    cls = type(name, (RomStruct,), clsvars)
+    cls = type(name, (Struct,), clsvars)
     return cls
