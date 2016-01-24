06795b76ef1913ca535de7fd5f3dfe29dd563578
Andrew Vant <ajvant@gmail.com>
2016-01-24 10:30:23 -0500

    Fixed pylint complaints in text.py.

--- src/romlib/text.py
+++ src/romlib/text.py
@@ -1,9 +1,18 @@
+""" Implements ROM text tables.
+
+The accepted format is defined in Nightcrawler's "Text Table Format" standard.
+
+I would be *really* happy if I could implement this using python's built in
+codec capabilities.
+"""
+
 from patricia import trie
 
 
 class TextTable(object):
+    """ A ROM text table, used for decoding and encoding text strings."""
     def __init__(self, name, f):
-        self.id = None
+        self.id = None  # pylint: disable=invalid-name
         self.name = name
         self.enc = trie()
         self.dec = trie()
@@ -41,28 +50,42 @@ class TextTable(object):
         if pos is not None:
             f.seek(pos)
         data = f.read(maxlen)
-        return self.decode(data, include_eos=include_eos)
+        return self.decode(data, include_eos, True)
 
-    def encode(self, s):
+    def readstr_bs(self, bs, pos=None, include_eos=True, maxlen=1024):
+        """ Read and decode an eos-terminated string from a bitstring.
+
+        Note that maxlen is *bytes*, not bits.
+        """
+        if pos is None:
+            pos = bs.pos
+        data = bs[pos:pos+maxlen]
+        return self.decode(data.bytes, include_eos, True)
+
+    def encode(self, string):
+        """ Encode a string into a series of bytes."""
         codeseq = []
         i = 0
-        while i < len(s):
-            match, code = self.enc.item(s[i:])
+        while i < len(string):
+            match, code = self.enc.item(string[i:])
             codeseq.extend(code)
             i += len(match)
         return bytes(codeseq)
 
     def decode(self, data, include_eos=True, stop_on_eos=True):
+        """ Decode a series of bytes.
+
+        Any unrecognized bytes will be rendered as hex codes.
+        """
         text = ""
         i = 0
         while i < len(data):
-            byte = data[i:i+1]
-            s = "[${:02X}]".format(data[i])
-            match, s = self.dec.item(data[i:], default=s)
+            raw = "[${:02X}]".format(data[i])
+            match, string = self.dec.item(data[i:], default=raw)
             if match is None:
                 match = data[i:i+1]
             if include_eos or match not in self.eos:
-                text += s
+                text += string
             if stop_on_eos and match in self.eos:
                 break
             i += len(match)
