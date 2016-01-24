ab3aecb9e4dac1caad85a1337a7ffdc2e08e9e3d
Andrew Vant <ajvant@gmail.com>
2016-01-24 11:20:59 -0500

    Fixed broken import in romlib.init.

--- src/romlib/__init__.py
+++ src/romlib/__init__.py
@@ -1,6 +1,6 @@
 from . import util, patch, rommap, struct, text
 from .rommap import RomMap, ArrayDef
-from .struct import Struct, StructDef
+from .struct import StructDef, Field
 from .patch import Patch
 from .text import TextTable
 
