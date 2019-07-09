920d2e05ec564794f2328ff428e77f4fb138d647
Andrew Vant <ajvant@gmail.com>
2019-07-08 22:48:49 -0400

    First stab at refactoring conf file loading

--- src/romtool/cli.py
+++ src/romtool/cli.py
@@ -6,7 +6,7 @@ import textwrap
 
 import yaml
 
-from romtool import commands
+import romtool.commands
 from romtool import util
 from romtool.util import pkgfile
 
@@ -34,6 +34,21 @@ def parser_setup(parser, spec):
             names = name.split("|")
             parser.add_argument(*names, **metaargs, help=desc)
 
+def load_conf_file(filename):
+    """ Load a project conf file """
+    options = {}
+    try:
+        with open(args.conf) as conffile:
+            options.update(yaml.load(conffile, Loader=yaml.SafeLoader))
+    except FileNotFoundError as e:
+        logging.error("Failed to load conf file. " + str(e))
+        exit(2)
+    logging.debug("Loaded args:")
+    for arg, default in defaults.items():
+        logging.debug("%s: %s", arg, default)
+    return options
+
+
 def main():
     """ Entry point for romtool."""
 
@@ -43,66 +58,48 @@ def main():
     # FIXME: After some thought, probably better to use one big string in the
     # source. :-(
 
-    logging.basicConfig()  # Do this here so it doesn't happen implicitly later
+    # Do this here so it doesn't happen implicitly later
+    # FIXME: replace with dictConfig. Set root in dictconfig, log
+    # individual files with module name as logger name.
+    logging.basicConfig()  
 
     with open(pkgfile("args.yaml")) as argfile:
         argspecs = yaml.load(argfile, Loader=yaml.SafeLoader)
 
-    # Set up the toplevel parser. This takes some magic. The conf file arg must
-    # be parsed before any others, and --help must be suppressed until after
-    # said processing. The simplest way I could find to do that is to create a
-    # "dummy" parser.
-    #
-    # For some reason, checking for -v here breaks concatenated short args,
-    # e.g.  -vf doesn't work. I think I can't mix concatenated args across
-    # multiple parsings, which means I can't support verbose here, only debug
-    # (which has no short form)
-
-    bootstrapper = argparse.ArgumentParser(add_help=False)
-    bootstrapper.add_argument("-c", "--conf")
-    bootstrapper.add_argument("--debug", action="store_true")
-    args, remainingargs = bootstrapper.parse_known_args()
-    if args.debug:
-        logging.getLogger().setLevel(logging.DEBUG)
-
+    # Set up CLI parser
+    # FIXME: function this out more. Or make a custom argumentparser
+    # class?
     globalargs = argspecs.pop("global")
     topparser = argparse.ArgumentParser(**globalargs.get('spec', {}))
     parser_setup(topparser, globalargs)
 
-    # Process the conf file if there is one; any options given in it become
-    # defaults, but can still be overridden on the rest of the command line.
-    defaults = {}
-    if args.conf:
-        logging.debug("Loading conf file '%s'", args.conf)
-        try:
-            with open(args.conf) as conffile:
-                defaults.update(yaml.load(conffile, Loader=yaml.SafeLoader))
-        except FileNotFoundError as e:
-            logging.error("Failed to load conf file. " + str(e))
-            exit(2)
-        logging.debug("Loaded args:")
-        for arg, default in defaults.items():
-            logging.debug("%s: %s", arg, default)
-    topparser.set_defaults(**defaults)
-
-    # Create the subparsers
     subparsers = topparser.add_subparsers(title="commands")
-    for command, argspec in sorted(argspecs.items()):
-        subparser = subparsers.add_parser(command,
-                                          conflict_handler='resolve',
-                                          **argspec.get("spec", {}))
-        parser_setup(subparser, argspec)
-        parser_setup(subparser, globalargs)
-        subparser.set_defaults(func=getattr(commands, command), **defaults)
+    for cmd, argspec in sorted(argspecs.items()):
+        sp = subparsers.add_parser(cmd, conflict_handler='resolve',
+                                   **argspec.get("spec", {}))
+        parser_setup(sp, argspec)
+        parser_setup(sp, globalargs)
+        sp.set_defaults(func=getattr(romtool.commands, cmd))
 
-    # Parse remaining arguments and add the results to the args namespace.
-    args = topparser.parse_args(args=remainingargs, namespace=args)
+    # Parse arguments
+    args = topparser.parse_args()
 
     # Set up logging
     if args.verbose:
         logging.getLogger().setLevel(logging.INFO)
     if args.debug:
         logging.getLogger().setLevel(logging.DEBUG)
+
+    # Read and apply project conf if supplied.
+    if args.conf:
+        logging.info("Loading conf file '%s'", args.conf)
+        conf = load_conf_file(args.conf)
+        for k, v in conf.items():
+            # This is supposed to fill in arguments from the conf file
+            # iff they were not given on the cli.
+            argdict = args.__dict__
+            if k in argdict and argdict[k] is None:
+                argdict[k] = v
 
     # If no subcommand supplied, print help.
     if not hasattr(args, 'func'):
