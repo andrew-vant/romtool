3446534f8b6d90745d8c731a9928a867e3919677
Andrew Vant <ajvant@gmail.com>
2020-02-17 21:08:12 -0500

    More tests

--- test/test_types.py
+++ test/test_types.py
@@ -6,11 +6,15 @@ from bitstring import BitStream
 
 class TestStructure(unittest.TestCase):
     def setUp(self):
-        self.data = BitStream(b'\x01\x02\x03\x04abcdef')
+        self.data = b'\x01\x02\x03\x04abcdef'
         self.fields = [{'fid': 'one',
                         'label': 'One Label',
                         'type': 'uint',
-                        'offset': '0'}]
+                        'offset': '0'},
+                       {'fid': 'two',
+                        'label': 'Two Label',
+                        'type': 'uint',
+                        'offset': 1}]
 
     def test_define_struct(self):
         structtype = Structure.define('scratch', self.fields, force=True)
@@ -18,27 +22,27 @@ class TestStructure(unittest.TestCase):
 
     def test_instantiate_struct(self):
         structtype = Structure.define('scratch', self.fields, force=True)
-        struct = structtype(self.data, 0)
+        struct = structtype(BitStream(self.data), 0)
         self.assertIsInstance(struct, structtype)
 
     def test_read_struct_attr(self):
         structtype = Structure.define('scratch', self.fields, force=True)
-        struct = structtype(self.data, 0)
+        struct = structtype(BitStream(self.data), 0)
         self.assertEqual(struct.one, 1)
 
     def test_read_struct_item_by_fid(self):
         structtype = Structure.define('scratch', self.fields, force=True)
-        struct = structtype(self.data, 0)
+        struct = structtype(BitStream(self.data), 0)
         self.assertEqual(struct['one'], 1)
 
     def test_read_struct_item_by_label(self):
         structtype = Structure.define('scratch', self.fields, force=True)
-        struct = structtype(self.data, 0)
+        struct = structtype(BitStream(self.data), 0)
         self.assertEqual(struct['One Label'], 1)
 
     def test_write_struct_attr(self):
         structtype = Structure.define('scratch', self.fields, force=True)
-        struct = structtype(self.data, 0)
+        struct = structtype(BitStream(self.data), 0)
         struct.one = 2
         self.assertEqual(struct.one, 2)
         self.assertEqual(struct['one'], 2)
@@ -46,7 +50,7 @@ class TestStructure(unittest.TestCase):
 
     def test_write_struct_item_by_fid(self):
         structtype = Structure.define('scratch', self.fields, force=True)
-        struct = structtype(self.data, 0)
+        struct = structtype(BitStream(self.data), 0)
         struct['one'] = 2
         self.assertEqual(struct.one, 2)
         self.assertEqual(struct['one'], 2)
@@ -55,6 +59,22 @@ class TestStructure(unittest.TestCase):
     def test_write_struct_stream_contents(self):
         # Test that writes do the right thing with the underlying stream
         structtype = Structure.define('scratch', self.fields, force=True)
-        struct = structtype(self.data, 0)
+        stream = BitStream(self.data)
+        struct = structtype(stream, 0)
         struct.one = 2
-        
+        stream.pos = 0
+        self.assertEqual(stream.bytes[0], 2)
+
+    def test_read_field_with_offset(self):
+        structtype = Structure.define('scratch', self.fields, force=True)
+        struct = structtype(BitStream(self.data), 0)
+        self.assertEqual(struct.two, 2)
+        self.assertEqual(struct['two'], 2)
+
+    def test_write_field_with_offset(self):
+        structtype = Structure.define('scratch', self.fields, force=True)
+        struct = structtype(BitStream(self.data), 0)
+        struct.two = 4
+        self.assertEqual(struct.two, 4)
+        self.assertEqual(struct['two'], 4)
+        self.assertEqual(struct['Two Label'], 4)
