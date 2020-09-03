b2fdbccbc588a919a67e73b325679eee9a9634ba
Andrew Vant <ajvant@gmail.com>
2020-09-03 19:13:09 -0400

    Clean up pylint, break primitives out again

---
+++ src/romlib/primitives.py
@@ -0,0 +1,107 @@
+from abc import ABC, abstractmethod
+
+from bitarray import bitarray
+from bitarray.util import ba2int, int2ba
+from fixedint import FixedInt
+from fixedint.util import HexFormattingMixin
+
+
+# Types in this module are expected to be immutable, low-level types that can
+# be read and written to a stream, or interoperate with built-in types. So far
+# these are all integrals.
+
+class Primitive(ABC):
+    """Interface for primitive types
+
+    All primitives must define these methods. They are used by structs, lists,
+    etc to read and write fields and items."""
+
+    @classmethod
+    @abstractmethod
+    def from_bits(cls, ba):
+        raise NotImplementedError
+
+    @abstractmethod
+    @classmethod
+    def from_stream(cls, ba):
+        raise NotImplementedError
+
+    @abstractmethod
+    def to_bits(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def to_stream(self, stream):
+        raise NotImplementedError
+
+
+class uint_mixin:
+    @classmethod
+    def from_bits(cls, ba):
+        return cls(ba2int(ba), width=len(ba), signed=False, mutable=False)
+
+    @classmethod
+    def from_stream(cls, stream):
+        return cls.from_bits(stream.readbits(cls.width))
+
+    def to_bits(self):
+        return int2ba(self, length=self.width)
+
+    def to_stream(self, stream):
+        stream.writebits(self.to_bits())
+
+    # Convenience properties
+    @property
+    def bits(self):
+        return self.to_bits()
+
+    @property
+    def bytes(self):
+        return self.to_bytes()
+
+
+class uintle_mixin(uint_mixin):
+    def to_bytes(self):
+        return super().to_bytes('little')
+
+    def to_bits(self):
+        _bytes = self.to_bytes()
+        ba = bitarray()
+        ba.frombytes(_bytes)
+        return ba
+
+    @classmethod
+    def from_bits(cls, ba):
+        return cls.from_bytes(ba.tobytes(), byteorder='little', signed=False)
+
+
+class uintbe_mixin(uint_mixin):
+    def to_bytes(self):
+        return super().to_bytes('big')
+
+    def to_bits(self):
+        _bytes = self.to_bytes()
+        ba = bitarray()
+        ba.frombytes(_bytes)
+        return ba
+
+    @classmethod
+    def from_bits(cls, ba):
+        return cls.from_bytes(ba.tobytes(), byteorder='big', signed=False)
+
+
+def uint_cls(tpname, sz_bits, fmt=None):
+    registry = {'uint': uint_mixin,
+                'uintle': uintle_mixin,
+                'uintbe': uintbe_mixin}
+
+    bases = []
+    bases.append(Primitive)
+    bases.append(FixedInt(width=sz_bits, signed=False, mutable=False))
+    if fmt == 'hex':
+        bases.append(HexFormattingMixin)  # Love that this is builtin
+    bases.append(registry[tpname])
+
+    clsname = f'{tpname}{sz_bits}'
+    newcls = type(clsname, tuple(reversed(bases)), {})
+    return newcls
--- src/romlib/types.py
+++ src/romlib/types.py
@@ -1,24 +1,18 @@
-import logging
-import abc
-from collections import Counter, ChainMap
-from types import SimpleNamespace
-from collections.abc import Mapping, Sequence
-from dataclasses import dataclass
-from fixedint import FixedInt
-from bitarray import bitarray
-from bitarray.util import int2ba, ba2int
-
-import bitstring
-import yaml
-import asteval
-
-from . import util
-
 """ Rom data structures
 
 These objects form a layer over the raw rom, such that accessing them
 automagically turns the bits and bytes into integers, strings, etc.
 """
+import logging
+import enum
+from types import SimpleNamespace
+from collections.abc import Mapping, Sequence
+
+import yaml
+
+from .primitives import uint_cls
+from . import util
+
 
 log = logging.getLogger(__name__)
 
@@ -28,7 +22,7 @@ class Origin(enum.Enum):
     parent = 'parent'
 
     def __contains__(self, item):
-        return item in self.__members__ or super().__contains__(item)
+        return item in type(self).__members__ or super().__contains__(item)
 
 
 class Unit(enum.IntEnum):
@@ -37,11 +31,11 @@ class Unit(enum.IntEnum):
     kb = 1024
 
     def __contains__(self, item):
-        return item in self.__members__ or super().__contains__(item)
+        return item in type(self).__members__
 
 
 class Offset:
-    def __init__(self, expr, unit=Unit.bytes, origin=Origin.relative):
+    def __init__(self, expr, unit=Unit.bytes, origin=Origin.parent):
         expr = str(expr)  # Just in case someone passes int
         self.expr = expr
         self.origin = origin
@@ -80,7 +74,7 @@ class Offset:
                 kwargs['unit'] = Unit[part]
             elif part in Origin:
                 kwargs['origin'] = Origin[part]
-            elif expr is None:
+            elif 'expr' not in kwargs:
                 kwargs['expr'] = part
             else:
                 raise ValueError(f"Invalid offset spec: {spec}")
@@ -92,19 +86,34 @@ class Size:
         self.count = count
         self.unit = unit
 
-        @property
-        def bits(self):
-            return self.count * self.scale
+    @property
+    def bits(self):
+        return self.count * self.unit
+
+    @classmethod
+    def define(cls, spec, **defaults):
+        parts = spec.split(':')
+        kwargs = defaults.copy()
+        for part in parts:
+            if part in Unit:
+                kwargs['unit'] = Unit[part]
+            elif 'expr' not in kwargs:
+                kwargs['expr'] = part
+            else:
+                raise ValueError(f"Invalid size spec: {spec}")
+        return cls(**kwargs)
+
 
 
 class Field:
-    def __init__(self, cls, offset, mod=None):
+    def __init__(self, cls, offset, size=None, mod=None):
         if isinstance(mod, str) and issubclass(cls, int):
             mod = int(mod, 0)
         self.cls = cls
         self.size = size
         self.mod = mod
         self.offset = offset
+        self.name = None  # Specified by set_name below
 
     def _resolve_offset(self, obj):
         origin = 0 if self.offset.absolute else obj.offset
@@ -131,8 +140,9 @@ class Field:
 
     @classmethod
     def define(cls, spec, **defaults):
+        spec = dict(**defaults, **spec)
         offset = Offset.define(spec['offset'])
-        size = Size.define(spec['size'])
+        size = Size.define(spec['size']) if 'size' in spec else None
         mod = spec['mod']
         tpname = spec['type']
 
@@ -203,7 +213,7 @@ class Structure(Mapping):
                     msg = f"field id or label '{name}.{ident}' shadows a built-in attribute"
                     raise ValueError(msg)
                 if ident in attrs or ident in labels:
-                    msg =  f"duplicated field id or label: '{name}.{ident}'"
+                    msg = f"duplicated field id or label: '{name}.{ident}'"
                     raise ValueError(msg)
             attrs[f.id] = Field.define(dct)
             labels[f.label] = f.id
@@ -259,92 +269,8 @@ class Table(Sequence):
         return len(self.index)
 
 
-# NOTE FOR MORNING: This won't work with fixedint, because I can't subclass
-# FixedInt. But I think I can do something similar myself. Make a fixedint
-# class of my own with a define() classmethod that returns a subclass with a
-# specific size, byte-endianness, etc. Nothing signed.
-#
-# When to enforce size? In __new__? Take a look at how fixedint does it.
-#
-# alternative, I can't subclass FixedInt, but I *can* subclass the
-# classes it returns...do so, and add the appropriate methods via multiple
-# inheritance. Will that work?
-
-def uint_cls(tpname, sz_bits, fmt=None):
-    registry = {'uint': uint_mixin,
-                'uintle': uintle_mixin,
-                'uintbe': uintbe_mixin}
-    if tpname not in registry:
-        return None
-
-    bases = [FixedInt(width=sz_bits, signed=False, mutable=False)]
-    if fmt == 'hex':
-        bases.append(fixedint.util.HexFormattingMixin)  # Love that this is builtin
-    bases.append(registry[tpname])
-    bases.reverse()
-    clsname = f'{tpname}{sz_bits}'
-
-    newcls = type(clsname, bases, {})
-    return newcls
-
-
-class uint_mixin:
-    @classmethod
-    def from_bits(cls, ba):
-        return cls(ba2int(ba), width=len(ba), signed=False, mutable=False)
-
-    @classmethod
-    def from_stream(cls, stream):
-        return cls.from_bits(stream.readbits(cls.width))
-
-    def to_bits(self):
-        return int2ba(self, length=self.width)
-
-    def to_stream(self, stream):
-        stream.writebits(self.to_bits())
-
-    # Convenience properties
-    @property
-    def bits(self):
-        return self.to_bits()
-
-    @property
-    def bytes(self):
-        return self.to_bytes()
-
-
-class uintle_mixin(uint):
-    def to_bytes(self, byteorder='little'):
-        return super().to_bytes(byteorder)
-
-    def to_bits(self):
-        _bytes = self.to_bytes()
-        ba = bitarray()
-        ba.frombytes(_bytes)
-        return ba
-
-    @classmethod
-    def from_bits(cls, ba):
-        return cls.from_bytes(ba.tobytes(), byteorder='little', signed=False)
-
-
-class uintbe_mixin(uint):
-    def to_bytes(self, byteorder='big'):
-        return super().to_bytes(byteorder)
-
-    def to_bits(self):
-        _bytes = self.to_bytes()
-        ba = bitarray()
-        ba.frombytes(_bytes)
-        return ba
-
-    @classmethod
-    def from_bits(cls, ba):
-        return cls.from_bytes(ba.tobytes(), byteorder='big', signed=False)
-
-
 class Array(Table):
-    def __init__(self, stream, factory, offset, count, stride, scale=8):
+    def __init__(self, stream, cls, offset, count, stride, scale=8):
         index = [(offset + stride * i) * scale
                  for i in range(count)]
-        super().__init__(stream, factory, index)
+        super().__init__(stream, cls, index)
