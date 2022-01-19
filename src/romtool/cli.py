3a34bf72871faf649a8672a0f9a98fff82101ee6
Andrew Vant <ajvant@gmail.com>
2022-01-18 21:26:49 -0500

    Merge cli files
    Don't think the separation is worth it. Nothing calls commands but the
    cli, at least for now. That may change if I ever add a gui or something.

--- src/romtool/cli.py
+++ src/romtool/cli.py
@@ -1,6 +1,64 @@
+"""romtool
+
+A tool for examining and modifying ROMs
+
+Usage:
+    romtool --help
+    romtool ident [-l|--long] [options] <roms>...
+    romtool dump [options] <rom> <moddir> [<patches>...]
+    romtool build [options] <rom> <input>...
+    romtool convert [options] <infile> <outfile>
+    romtool apply [options] <rom> <patches>...
+    romtool diff [options] <original> <modified>
+    romtool fix [options] <rom>
+    romtool charmap <rom> <strings>...
+    romtool initchg <rom> <filename>
+    romtool dirs
+
+Commmands:
+    ident               Print information about a ROM file
+    dump                Dump all known data from a ROM to `moddir`
+    build               Construct a patch from input files
+    convert             Convert a patch from one format to another
+    apply               Apply patches to a ROM
+    diff                Construct a patch by diffing two ROMs
+    fix                 Fix bogus headers and checksums
+    charmap             Generate a texttable from known strings
+    initchg             Generate a starter changeset file.
+    dirs                Print directory paths used by romtool
+
+Options:
+    -i, --interactive   Prompt for confirmation on destructive operations
+    -n, --dryrun        Show what would be done, but don't do it
+    -f, --force         Never ask for confirmation
+
+    -o, --out PATH      Output file or directory. Detects type by extension
+    -m, --map PATH      Manually specify rom map
+    -S, --sanitize      Include internal checksum updates in patches
+    -N, --nobackup      Don't create backup when patching files
+
+    -h, --help          Print this help
+    -V, --version       Print version and exit
+    -v, --verbose       Verbose output
+    -q, --quiet         Quiet output
+    -D, --debug         Even more verbose output
+    --pdb               Start interactive debugger on crash
+
+    -l, --long          Print additional ROM information
+
+Examples:
+    A simple modding session looks like this:
+
+    $ romtool dump game.rom projectdir
+    # <edit the files in projectdir with a spreadsheet program>
+    $ romtool build game.rom projectdir -o game.ips
+"""
+
 import sys
-import hashlib
 import logging
+import logging.config
+import textwrap
+import hashlib
 import os
 import shutil
 import itertools
@@ -12,38 +70,34 @@ from textwrap import dedent
 
 from addict import Dict
 from appdirs import AppDirs
+from docopt import docopt
 
 import romtool
-from .. import charset
-from .. import config
-from ..rommap import RomMap
-from ..rom import Rom
-from ..patch import Patch
-from ..util import pkgfile, slurp, loadyaml
-from ..util import pipeline, readtsv
-from ..exceptions import ChangesetError
-
+from . import util
+from . import config
+from .rommap import RomMap
+from .rom import Rom
+from .patch import Patch
+from .util import pkgfile, slurp, loadyaml
+from .util import pipeline
+from .version import version
+from .exceptions import RomtoolError, RomDetectionError, ChangesetError
 
 log = logging.getLogger(__name__)
 
+try:
+    # Try to do the right thing when piping to head, etc.
+    from signal import signal, SIGPIPE, SIG_DFL
+    signal(SIGPIPE, SIG_DFL)
+except ImportError:
+    # SIGPIPE isn't available on Windows, at least not on my machine. For now
+    # just ignore it, but I should probably test piping on windows at some
+    # point.
+    pass
 
-class RomDetectionError(Exception):
-    """ Indicates that we couldn't autodetect the map to use for a ROM."""
-    def __init__(self, _hash=None, filename=None):
-        super().__init__()
-        self.hash = _hash
-        self.filename = filename
-    def __str__(self):
-        return "ROM sha1 hash not in db: {}".format(self.hash)
-    def log(self):
-        log.error("Couldn't autodetect ROM map for %s", self.filename)
-        log.error("%s", self)
-        log.error("The rom may be unsupported, or your copy may "
-                      "be modified, or this may be a save file")
-        log.error("You will probably have to explicitly supply --map")
 
 # FIXME: Add subcommand to list available maps on the default search paths.
-# Probably its output should advice the user that it's only what shipped with
+# Probably its output should advise the user that it's only what shipped with
 # romtool and they're free to supply their own.
 
 def detect(romfile, maproot=None):
@@ -382,13 +436,13 @@ def ident(args):
 
 
 def dirs(args):
-    dirs = AppDirs("romtool")
+    ad = AppDirs("romtool")
     out = f"""
-        config:     {dirs.user_config_dir}
-        data:       {dirs.user_data_dir}
-        state:      {dirs.user_state_dir}
-        cache:      {dirs.user_cache_dir}
-        logs:       {dirs.user_log_dir}
+        config:     {ad.user_config_dir}
+        data:       {ad.user_data_dir}
+        state:      {ad.user_state_dir}
+        cache:      {ad.user_cache_dir}
+        logs:       {ad.user_log_dir}
         """
     print(dedent(out).strip())
 
@@ -425,3 +479,83 @@ def _writepatch(patch, outfile):
     else:
         patch.to_ipst(sys.stdout)
     log.info("There were %s changes.", len(patch.changes))
+
+class Args(Dict):
+    """ Convenience wrapper for the docopt dict
+
+    This exists so I can do args.whatever and get the Right Thing out of it.
+    """
+
+    keyfmts = ['{key}',
+               '-{key}',
+               '--{key}',
+               '<{key}>']
+
+    def _realkey(self, key):
+        # Look for the first key-variant that's present, otherwise use the
+        # original key.
+        for fmt in type(self).keyfmts:
+            realkey = fmt.format(key=key)
+            if realkey in self:
+                return realkey
+        return key
+
+    def __getitem__(self, key):
+        key = self._realkey(key)
+        return super().__getitem__(key)
+
+    def __setitem__(self, key, value):
+        key = self._realkey(key)
+        super().__setitem__(key, value)
+
+    @property
+    def command(self):
+        return next(k for k, v in self.items()
+                    if k.isalnum() and v)
+
+
+def initlog(args):
+    key = ('debug' if args.debug
+           else 'verbose' if args.verbose
+           else 'quiet' if args.quiet
+           else 'default')
+    logconf = config.load('logging.yaml')
+    logging.config.dictConfig(logconf[key])
+
+
+def main(argv=None):
+    """ Entry point for romtool."""
+
+    args = Args(docopt(__doc__, argv, version=version))
+    initlog(args)
+    util.debug_structure(args)
+
+    expected = (FileNotFoundError, NotImplementedError, RomtoolError) if not args.debug else ()
+
+    try:
+        globals()[args.command](args)
+    except KeyboardInterrupt as ex:
+        log.error(f"keyboard interrupt; aborting")
+        sys.exit(2)
+    except expected as ex:
+        # I'd rather not separately handle this in every command that uses it.
+        log.error(ex)
+        sys.exit(2)
+    except Exception as ex:  # pylint: disable=broad-except
+        # I want to break this into a function and use it as excepthook, but
+        # every time I try it doesn't work.
+        log.exception(ex)
+        if not args.pdb:
+            sys.exit(2)
+        import pdb
+        print("\n\nCRASH -- UNHANDLED EXCEPTION")
+        msg = ("Starting debugger post-mortem. If you got here by "
+               "accident (perhaps by trying to see what --pdb does), "
+               "you can get out with 'quit'.\n\n")
+        print("\n{}\n\n".format("\n".join(textwrap.wrap(msg))))
+        pdb.post_mortem()
+        sys.exit(2)
+
+
+if __name__ == "__main__":
+    main()
--- src/romtool/exceptions.py
+++ src/romtool/exceptions.py
@@ -11,3 +11,18 @@ class MapError(RomtoolError):
 
 class ChangesetError(RomtoolError):
     """ Exception raised for broken changesets """
+
+class RomDetectionError(RomtoolError):
+    """ Indicates that we couldn't autodetect the map to use for a ROM."""
+    def __init__(self, _hash=None, filename=None):
+        super().__init__()
+        self.hash = _hash
+        self.filename = filename
+    def __str__(self):
+        return "ROM sha1 hash not in db: {}".format(self.hash)
+    def log(self):
+        log.error("Couldn't autodetect ROM map for %s", self.filename)
+        log.error("%s", self)
+        log.error("The rom may be unsupported, or your copy may "
+                      "be modified, or this may be a save file")
+        log.error("You will probably have to explicitly supply --map")
